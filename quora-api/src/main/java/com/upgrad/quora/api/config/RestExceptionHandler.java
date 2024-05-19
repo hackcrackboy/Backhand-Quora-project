906c63d885f9d5ee5bfb0784c1c8145fdc2bad17
hackcrackboy <piyushgoyal1234512345@gmail.com>
2024-05-19 15:48:11 +0530

    first commit

---
+++ quora-api/src/main/java/com/upgrad/quora/api/config/RestExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.upgrad.quora.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> resourceNotFoundException(Exception exe, WebRequest request){
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND
+        );
+    }
+}
